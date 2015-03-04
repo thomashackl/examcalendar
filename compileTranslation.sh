d8f410956489dc1508973cd182661ac6166ac9ea
Thomas Hackl <thomas.hackl@uni-passau.de>
2015-03-04 16:04:25 +0100

    correct language domain setting

--- compileTranslation.sh
+++ compileTranslation.sh
@@ -9,6 +9,6 @@ LOCALE_RELATIVE_PATH="locale"
 
 for language in en
 do
-    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.mo" && mv "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.mo" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.mo.old"
-    msgfmt "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.po" --output-file="$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.mo"
+    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.mo" && mv "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.mo" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.mo.old"
+    msgfmt "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.po" --output-file="$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.mo"
 done
--- extractTranslationStrings.sh
+++ extractTranslationStrings.sh
@@ -10,10 +10,10 @@ TRANSLATIONFILES_RELATIVE_PATHS="."
 
 for language in en
 do
-    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.po" && mv "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.po" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.po.old"
-    > "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.UTF-8.po"
-    find $TRANSLATIONFILES_RELATIVE_PATHS \( -iname "*.php" \) | xargs xgettext --from-code=ISO-8859-1 -j -n --language=PHP -o "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.UTF-8.po"
-    msgconv --to-code=iso-8859-1 "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.UTF-8.po" -o "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.po"
-    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.po.old" && msgmerge "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.po.old" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.po" --output-file="$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.po"
-    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.UTF-8.po" && rm "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendarplugin.UTF-8.po"
+    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.po" && mv "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.po" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.po.old"
+    > "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.UTF-8.po"
+    find $TRANSLATIONFILES_RELATIVE_PATHS \( -iname "*.php" \) | xargs xgettext --from-code=ISO-8859-1 -j -n --language=PHP -o "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.UTF-8.po"
+    msgconv --to-code=iso-8859-1 "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.UTF-8.po" -o "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.po"
+    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.po.old" && msgmerge "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.po.old" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.po" --output-file="$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.po"
+    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.UTF-8.po" && rm "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/examcalendar.UTF-8.po"
 done
